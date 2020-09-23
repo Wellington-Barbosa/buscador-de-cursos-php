3c2a4b7c9d66749b13de922207182ce2aaaecb0e
WellyBarbosa <wellingtonwelly123@gmail.com>
2020-09-23 15:58:26 -0300

    Primeiro Commit

---
+++ src/Buscador.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WellingtonBarbosa\Projeto;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url): array
+    {
+        $resposta = $this->httpClient->request('GET', $url, ['verify' => false]);
+
+        $html = $resposta->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent . PHP_EOL;
+        }
+        return $cursos;
+    }
+}
